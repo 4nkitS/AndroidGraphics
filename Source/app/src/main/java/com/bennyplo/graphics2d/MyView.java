0aa149bb97c610ad4bff4e489228e99c3f15550c
Ankit Shukla <shukla.ankit@protonmail.com>
2021-03-26 05:58:12 -0400

    2D Shapes

--- Source/app/src/main/java/com/bennyplo/graphics2d/MyView.java
+++ Source/app/src/main/java/com/bennyplo/graphics2d/MyView.java
@@ -10,16 +10,23 @@ import android.view.View;
  */
 
 public class MyView extends View {
-    private Paint redPaint;
+    private Paint whitePaint;
 
+    private Paint bluePaint;
 
     public MyView(Context context) {
         super(context, null);
         //Add your initialisation code here
-        redPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
-        redPaint.setStyle(Paint.Style.STROKE);//stroke only no fill
-        redPaint.setColor(0xffff0000);//color red
-        redPaint.setStrokeWidth(5);//set the line stroke width to 5
+        whitePaint =new Paint(Paint.ANTI_ALIAS_FLAG);
+        whitePaint.setStyle(Paint.Style.STROKE);//stroke only no fill
+        whitePaint.setColor(0xffffffff);//color red
+        whitePaint.setStrokeWidth(5);//set the line stroke width to 5
+
+        bluePaint =new Paint(Paint.ANTI_ALIAS_FLAG);
+        bluePaint.setStyle(Paint.Style.STROKE);//stroke only no fill
+        bluePaint.setColor(0xff0000ff);//color black
+        bluePaint.setTextSize(80);
+        bluePaint.setStrokeWidth(12);//set the line stroke width to 5
     }
 
 
@@ -27,6 +34,15 @@ public class MyView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         //Add your drawing code here
-        canvas.drawRect(10,30,200,200,redPaint);
+        canvas.drawRect(10,30,200,200, bluePaint);
+        canvas.drawARGB(200, 150, 150, 250);
+
+        canvas.drawCircle(300,300,250,bluePaint);
+        canvas.drawCircle(500,450,50,bluePaint);
+
+        canvas.drawRect(500,500,700,700, bluePaint);
+        canvas.drawCircle(600,600,144,bluePaint);
+
+        canvas.drawText("Ankit Shukla", 50, 80, bluePaint);
     }
 }
